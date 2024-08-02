cf3f803e8e93c705a457caa9ed4d3856063385f5
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-08-03 00:17:46 +1000

    add event service

---
+++ src/services/eventService.js
@@ -0,0 +1,107 @@
+const BASE_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL + "/events";
+
+const index = async () => {
+  try {
+    const response = await fetch(BASE_URL, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const show = async (id) => {
+  try {
+    const response = await fetch(BASE_URL + "/" + id, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const create = async (newEvent) => {
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newEvent),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const update = async (id, eventFormData) => {
+  try {
+    const response = await fetch(BASE_URL + "/" + id, {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(eventFormData),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteEvent = async (id) => {
+  try {
+    const response = await fetch(BASE_URL + "/" + id, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const joinEvent = async (eventId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${eventId}/join`, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+const leaveEvent = async (eventId) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${eventId}/leave`, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+    });
+    // return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+export { index, show, create, update, deleteEvent, joinEvent, leaveEvent };
