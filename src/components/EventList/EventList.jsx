6eb137181c6671f0cd9af2eeea5a2eceb34b1ccf
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-08-04 22:41:02 +1000

    update event list

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -5,7 +5,7 @@ const EventList = (props) => {
   const eventListItems = props.events.map((event) => (
     <Link
       key={event._id}
-      className="card position-relative"
+      className="card position-relative card-div"
       style={{ width: "18rem", minHeight: "400px" }}
       to={"/events/" + event._id}
     >
@@ -16,23 +16,25 @@ const EventList = (props) => {
       ></img>
       <div className="card-body">
         <h5 className="card-title">{event.title}</h5>
-        <h6 className="card-title">{event.cost ? "$"+event.cost : "FREE"}</h6>
-        <p className="card-text pb-4">{event.description}</p>
-        <div className="position-absolute bottom-0">
-          {event.attendees.length !== 0 ? (
+        <p>{event.description}</p>
+
+        {/* <p className="card-text">{event.date.substring(0, 10)}</p> */}
+        <p style={{ marginBottom: "0" }}>07 AUG 2024</p>
+        <div className="card-footer-div">
+          <div>
             <p
-              style={{
-                backgroundImage: "url('flame-icon.svg')",
-                backgroundPosition: "center left",
-                backgroundRepeat: "no-repeat",
-                paddingLeft: "25px",
-              }}
+              style={
+                {
+                  // paddingLeft: "25px",
+                }
+              }
             >
-              {event.attendees.length + " going" }
+              {event.attendees.length + " going"}
             </p>
-          ) : (
-            <p>No attendees yet</p>
-          )}
+          </div>
+          <div>
+            <p>{event.cost ? "$" + event.cost : "FREE"}</p>
+          </div>
         </div>
       </div>
     </Link>
