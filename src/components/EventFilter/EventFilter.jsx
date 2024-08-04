776da65e8ff91299453c0bca36f1b49b0608996b
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-08-04 22:40:50 +1000

    update filter

--- src/components/EventFilter/EventFilter.jsx
+++ src/components/EventFilter/EventFilter.jsx
@@ -26,8 +26,8 @@ const EventFilter = (props) => {
   };
 
   return (
-    <div className="nav d-flex gap-2 sidebar-filter">
-      <div className="mb-3">
+    <div className="nav d-flex gap-5 mb-4">
+      <div className="d-flex align-items-center gap-3">
         <label htmlFor="category-input" className="form-label">
           Category
         </label>
@@ -49,7 +49,7 @@ const EventFilter = (props) => {
           <option value="Social Activities">Social Activities</option>
         </select>
       </div>
-      <div className="mb-3">
+      <div className="d-flex align-items-center gap-3">
         <label htmlFor="type-input" className="form-label">
           Type
         </label>
@@ -65,18 +65,21 @@ const EventFilter = (props) => {
           <option value="In person">In person</option>
         </select>
       </div>
-      <div className="mb-3">
-        <label htmlFor="only-free-input" className="form-label">
-          Show Free events only
-        </label>
-
-        <input
-          type="checkbox"
-          id="only-free-input"
-          name="onlyFree"
-          checked={filterData.onlyFree}
-          onChange={handleChange}
-        />
+      <div className="d-flex align-items-center gap-3">
+        <div class="form-check form-switch">
+          <input
+            role="button"
+            className="form-check-input"
+            type="checkbox"
+            id="only-free-input"
+            name="onlyFree"
+            checked={filterData.onlyFree}
+            onChange={handleChange}
+          ></input>
+          <label className="form-check-label" htmlFor="only-free-input">
+            Show free events only
+          </label>
+        </div>
       </div>
     </div>
   );
